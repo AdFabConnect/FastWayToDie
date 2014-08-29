c8ccc5b99261feeca2d5481e2c7254cfe53e3ea8
nicolaslabbe <nicolas.labbe@adfab.fr>
2014-08-29 08:18:06 +0200

    FASTWAYTODIE-1

--- src/earth.js
+++ src/earth.js
@@ -1,22 +1,50 @@
 var rng = require('./rng');
+var raf = require('./raf');
 
 module.exports = function() {
 
 	var Ball = function(x, y) {
+		var color = rng().randHex();
+
 		this.div = document.createElement('div');
-		this.div.style.background = rng().randHex();
-		this.div.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
+		this.div.style.background = color;
+		this.div.style.borderBottomColor = color;
+		this.div.style.borderTopColor = color;
+		this.div.style.top = y + 'px';
+		this.div.style.left = x + 'px';
+		this.div.style.zIndex = '999';
 		this.div.className = 'e-ball';
 
 		document.body.appendChild(this.div);
 
 		this.div.addEventListener('click', function(e) {
-			this.div.remove(this);
+			earth.destroyBall(this.div);
 		}.bind(this));
 	};
 
 	var earth = {
 		size: 100,
+
+		destroyBall: function(div) {
+			var i = 0;
+
+			div.classList.add('e-boom');
+			div.classList.add('scale');
+
+			raf.start(function() {
+				if (i === 30) {
+					div.remove(this);
+					return;
+				}else if (i === 15) {
+					div.classList.remove('e-end');
+					div.style.background = 'none';
+					div.classList.add('e-end');
+				}else if (i === 10) {
+					div.classList.add('e-boom');
+				}
+				i++;
+			}.bind(this));
+		},
 
 		init:function() {
 
