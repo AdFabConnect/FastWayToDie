5ee0a9d4e728d1bf9ed0846c48f8ebd3b010563f
fabrice <fabrice.labbe@adfab.fr>
2014-09-04 22:40:44 +0200

    add visual timer

--- src/timer.js
+++ src/timer.js
@@ -1,14 +1,30 @@
 module.exports = {
 
   timer: null,
+  timerElement: null,
+
+  create: function() {
+    this.timerElement = document.createElement('div');
+    this.timerElement.id = 'counter-time';
+    document.body.appendChild(this.timerElement);
+  },
 
   start: function(duration, callBack) {
+
+    if(!this.timerElement){
+      this.create();
+    }
+
     if (this.timer){
       this.stop();
     }
+    
+    var timerElement = this.timerElement;
+
+    this.timerElement.innerHTML = 0;
 
     this.timer = setInterval(function() {
-      console.log(duration);
+      timerElement.innerHTML = duration;
       if (duration-- < 0){
         callBack();
       }
@@ -16,6 +32,7 @@ module.exports = {
   },
 
   stop: function() {
+    this.timerElement.innerHTML = '';
     clearInterval(this.timer);
     this.timer = null;
   }
