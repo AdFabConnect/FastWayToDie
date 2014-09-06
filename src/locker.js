32dadd6e436829ec9c1ac6489fe379c9d4efb50f
fabrice <fabrice.labbe@adfab.fr>
2014-09-06 15:23:45 +0200

    css3 lock portrait or landscape class

---
+++ src/locker.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+
+  lockPortrait: function(text) {
+    var b = document.body;
+    if(!b.classList.contains('lock-p')){
+      b.classList.add('lock-p')
+    }
+  },
+
+  locklandscape: function(text) {
+    var b = document.body;
+    if(!b.classList.contains('lock-l')){
+      b.classList.add('lock-l')
+    }
+  },
+
+  unlock: function(text) {
+    var b = document.body;
+    if(b.classList.contains('lock-l')){
+      b.classList.remove('lock-l')
+    }
+    if(b.classList.contains('lock-p')){
+      b.classList.remove('lock-p')
+    }
+  }
+
+};
