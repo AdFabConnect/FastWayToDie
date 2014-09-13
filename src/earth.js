5fc402147b74fa22a4fd6019bf88372c78150a9a
nicolaslabbe <nicolas.labbe@adfab.fr>
2014-09-13 10:15:06 +0200

    FastWayToDie-1 eslint

--- src/earth.js
+++ src/earth.js
@@ -1,6 +1,7 @@
 var rng = require('./rng');
 var raf = require('./raf');
 var Avatar = require('./avatar');
+var timer = require('./timer');
 
 module.exports = function(callbackEnd) {
 
@@ -8,19 +9,17 @@ module.exports = function(callbackEnd) {
 		var color = rng().randHex();
 
 		this.div = document.createElement('div');
-		this.div.innerHTML = '<div class="e-ball" style="background:'+color
-			+'; border-bottom-color:'+color
-			+'; border-top-color:'+color+'"></div>'
+		this.div.innerHTML = '<div class="e-ball" style="background:' + color + '; border-bottom-color:' + color + '; border-top-color:' + color + '"></div>';
 		// this.div.style.background = color;
 		// this.div.style.borderBottomColor = color;
 		// this.div.style.borderTopColor = color;
-		this.div.style.webkitTransform = 'scale(.'+(Math.round(Math.random()*2, 1)+5)+')';
+		this.div.style.webkitTransform = 'scale(.' + (Math.round(Math.random() * 2, 1) + 5) + ')';
 		this.div.style.top = y + 'px';
 		this.div.style.left = x + 'px';
 		this.div.style.zIndex = '999';
 		this.div.className = 'e-ball-wrap';
 
-		this.div.addEventListener('click', function(e) {
+		this.div.addEventListener('click', function() {
 			callback(this.div);
 		}.bind(this));
 
@@ -36,6 +35,14 @@ module.exports = function(callbackEnd) {
 		numb: 3,
 		balls: [],
 
+		aniDie: function() {
+			Avatar.getAvatar().removeEventListener('webkitAnimationEnd', this.aniDieFn);
+
+			Avatar.getAvatar().classList.add('die');
+			Avatar.getAvatar().classList.add('run');
+			this.win(true);
+		},
+
 		destroyBall: function(div) {
 			var i = 0,
 				ball = div.querySelector('.e-ball');
@@ -46,21 +53,13 @@ module.exports = function(callbackEnd) {
 			var id = raf.start(function() {
 				if (i === 30) {
 					div.remove(this);
-					if(this.life == 1) {
+					if (this.life === 1) {
 						Avatar.getAvatar().classList.add('die');
 						Avatar.getAvatar().classList.add('run');
 
-						function aniDie(arguments) {
-							Avatar.getAvatar().removeEventListener('webkitAnimationEnd', aniDieFn);
+						this.aniDieFn = this.aniDie.bind(this);
 
-							Avatar.getAvatar().classList.add('die');
-							Avatar.getAvatar().classList.add('run');
-							this.win(true);
-						}
-
-						var aniDieFn = aniDie.bind(this);
-
-						Avatar.getAvatar().addEventListener('webkitAnimationEnd', aniDieFn);
+						Avatar.getAvatar().addEventListener('webkitAnimationEnd', this.aniDieFn);
 					}
 					this.life--;
 					raf.stop(id);
@@ -81,9 +80,15 @@ module.exports = function(callbackEnd) {
 
 			this.setAvatar();
 			this.initGame(this.numb);
+
+		  timer.start([15, 7, 4][parseInt(window.levelIndex / window.gamesLength)], function() {
+		    this.win(false);
+		  }.bind(this)); 
 		},
 
 		win: function(bool) {
+      timer.stop();
+      
 			this.remove.call(this);
 			callbackEnd(bool);
 		},
@@ -101,16 +106,16 @@ module.exports = function(callbackEnd) {
 			
 			for (var i = 0; i < cpt; i++) {
 				// var x = document.body.offsetWidth/2 + (Math.round(Math.random() * (av * 3))) - (av * 3 / 2) - 40,
-				var x = document.body.offsetWidth/2 + (Math.round(Math.random() * (av))) - (av / 2) - 60,
+				var x = document.body.offsetWidth / 2 + (Math.round(Math.random() * (av))) - (av / 2) - 60,
 				//var x = Math.round(Math.random() * (document.body.offsetWidth - this.size)),
 					y = Math.round(Math.random() * 20) - 40;
 
 				var b = new Ball(x, y, this.destroyBall.bind(this));
 
-				var deltaY = 150 - y + (b.offsetHeight / 2);
-				var deltaX = document.body.offsetWidth / 2 - (x - (b.offsetWidth / 2));
+				// var deltaY = 150 - y + (b.offsetHeight / 2);
+				// var deltaX = document.body.offsetWidth / 2 - (x - (b.offsetWidth / 2));
 
-				var angle = (Math.atan(deltaX/deltaY) * 180 / Math.PI + 180);
+				// var angle = (Math.atan(deltaX / deltaY) * 180 / Math.PI + 180);
 
 				this.balls.push(b);
 
